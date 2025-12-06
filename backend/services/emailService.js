724d35cfd3f5a8110045cd10048c1b82fe89131f
DatNT12052003 <thanhdat2003pt@gmail.com>
2025-12-06 12:58:01 +0700

    fix error send otp with email

--- backend/services/emailService.js
+++ backend/services/emailService.js
@@ -3,9 +3,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    host: "sandbox.smtp.mailtrap.io",
+    port: 587,
+    // secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
